81a4f261439a2cfd876a25e7c2a359a9b8450bc9
HeTianYu12138 <895562423@qq.com>
2025-03-09 13:49:18 +0800

    Changes

---
+++ 三数之和.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public class 三数之和 {
+    public List<List<Integer>> threeSum(int[] nums) {
+        
+    }
+}
---
+++ 移动零.java
@@ -0,0 +1,17 @@
+public class 移动零 {
+    public void moveZeroes(int[] nums) {
+        int i = 0;
+        int j = 0;
+        while (j < nums.length) {
+            if (nums[j] != 0) {
+                nums[i] = nums[j];
+                i++;
+            }
+            j++;
+        }
+        while (i < nums.length) {
+            nums[i] = 0;
+            i++;
+        }
+    }
+}
